3fec529e427601aef41a05eeb226b183ea05827f
Cosmo Martinez <cosmo@ucsc.edu>
2019-06-10 09:47:45 -0700

    moves accessResponse definition into adobe_jwt.go

--- actions/actions.go
+++ actions/actions.go
@@ -1,14 +1,5 @@
 package actions
 
-// accessResponse is the json body of the response from
-// requestAccess. It contains the accessToken that is used
-// for authorizing User Management API requests.
-type accessResponse struct {
-	TokenType   string `json:"token_type"`
-	AccessToken string `json:"access_token"`
-	ExpiresIn   int    `json:"expires_in"`
-}
-
 // token is the current JWT for authenticating requests
 var token *accessResponse
 
--- actions/adobe_jwt.go
+++ actions/adobe_jwt.go
@@ -15,6 +15,15 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
+// accessResponse is the json body of the response from
+// requestAccess. It contains the accessToken that is used
+// for authorizing User Management API requests.
+type accessResponse struct {
+	TokenType   string `json:"token_type"`
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+}
+
 func generateJWT() string {
 	signBytes, err := ioutil.ReadFile(config.Details.Enterprise["PrivKeyPath"])
 	if err != nil {
